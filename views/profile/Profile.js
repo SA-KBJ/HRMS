56d7136ead7dc7cd03b6e243d430341775b038da
bansaripatel257 <bansari.patel.sa@gmail.com>
2019-02-07 00:16:30 +0530

    commit with tab

--- views/profile/Profile.js
+++ views/profile/Profile.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React , {Component} from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import colors from '../../config/colors';
 import dimen from '../../config/dimen';
 import CommonStyle from "../../style/comman";
+import { TabView, TabBar, SceneMap } from 'react-native-tab-view';
+import {Tab} from '../../config/navigations';
 
 
-class Profile extends React.Component {
+class Profile extends React.Component{
+    
     render() {
         return (
-            <View style={CommonStyle.container}>
-                <View style={CommonStyle.verticalView}>
+            <View style={CommonStyle.container} >
                 <View style={styles.detailView}>
                     <Image source={{ uri: "https://www.t-nation.com/system/publishing/articles/10005529/original/6-Reasons-You-Should-Never-Open-a-Gym.png" }} style={styles.profileImg} />
                 </View>
                 <View style={styles.tabView}>
-                    <Text>Hello</Text>
-                </View>
+                    <Tab />
                 </View>
             </View>
         );
+        // return <Tab />
     }
 }
 
@@ -27,28 +29,23 @@ const styles = StyleSheet.create({
         height: 80,
         width: 80,
         borderRadius: 40,
-    },
+        marginTop:30,
+
+        },
     detailView: {
         flex:1,
-        flexDirection: 'column',
+        alignSelf: 'stretch',
         backgroundColor: 'powderblue',
-       
-    },
-    innerView: {
-        flex: 1,
-        
-        justifyContent:'space-evenly',
-        marginTop: dimen.marginLarge,
-        marginBottom:dimen.marginLarge,
-        flexDirection: 'column',
-        backgroundColor: '#F5FCFF',
-    },
+        flexDirection:'column',
+        alignItems: 'center'
 
+
+    },
     tabView: {
-        flex:1,
-        flexDirection: 'column',
+        flex: 1,
+        alignSelf: 'stretch',
         backgroundColor: 'skyblue',
-            
+
     }
 })
 
